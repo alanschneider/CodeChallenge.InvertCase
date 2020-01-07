59c457b7320c97419da305a6b69d11e96695d0ba
Alan Schneider <aschneider@us.imshealth.com>
2020-01-07 15:23:38 -0800

    Doesn't seem like the fixture is actually looking at the target. I can't fail the debug test with a bad translation (ie set all the chars to null). Need to look at this later...

--- CodeChallenge.Target/Program.cs
+++ CodeChallenge.Target/Program.cs
@@ -5,26 +5,32 @@ namespace CodeChallenge.Target
 {
     class Program
     {
+        private static char[] arr = new char[128];
+
+        private static void InitializeMapping()
+        {
+            for (var c = '\0'; c <= 127; ++c)
+            {
+                if (c >= 65 && c <= 90) arr[c] = (char)(c + 32);
+                else if (c >= 97 && c <= 122) arr[c] = (char)(c - 32);
+                else arr[c] = c;
+            }
+        }
+
+        private static string InverseCase(string line) =>
+            new string(line.ToCharArray().Select(c => arr[c]).ToArray());
+
         static void Main(string[] args)
         {
-            // Protocol:
-            // => "#BEGIN\r\n"
-            // <= "#READY\r\n"
-            // => "ABCdef\r\n"
-            // <= "abcDEF\r\n"
-            // (...)
-            // => "#END 0\r\n"
+            InitializeMapping();
 
-            // Output windows newlines
             Console.Out.NewLine = "\r\n";
             string line;
 
             while ((line = ReadLineTee()) == "#BEGIN")
             {
                 Console.WriteLine("#READY");
-
-                while (!(line = ReadLineTee()).StartsWith("#END"))
-                {
+                while (!(line = ReadLineTee()).StartsWith("#END")) {
                     Console.WriteLine(InverseCase(line));
                 }
             }
@@ -35,27 +41,6 @@ namespace CodeChallenge.Target
             var line = Console.In.ReadLine();
             //Console.Error.WriteLine(line);
             return line;
-        }
-
-        private static string InverseCase(string line)
-        {
-            return new string(line.ToCharArray().Select(InverseCase).ToArray());
-        }
-
-        private static char InverseCase(char c)
-        {
-            if (char.IsUpper(c))
-            {
-                return char.ToLower(c);
-            }
-            else if (char.IsLower(c))
-            {
-                return char.ToUpper(c);
-            }
-            else
-            {
-                return c;
-            }
         }
     }
 }
